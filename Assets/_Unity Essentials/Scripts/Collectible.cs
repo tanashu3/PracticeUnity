44a66b91420d02b807f36cff1e50c39021fcfbd7
Tanashu <tanashu3@gmail.com>
2025-08-17 15:22:14 +0900

    dirtが消えるようになった

---
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,rotationSpeed,0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Destroy the collectible
+        Destroy(gameObject);
+    }
+
+}
